71a5a53f20e4c0d1b44035563d56771ba7f0a9db
Moses Palacios <mospalacios@gmail.com>
2020-04-07 21:41:45 -0400

    created navbar and employee card components

---
+++ client/src/components/EmployeeCard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function EmplyeeCard({ children }) {
+    return (
+      <div className="list-overflow-container">
+        <ul className="list-group">{children}</ul>
+      </div>
+    );
+  };
+
+  export function CardItem({ children }) {
+    return <li className="list-group-item">{children}</li>;
+  };
+  
+  
---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav()  {
+    return (
+        <div className = "navbar navbar-expand-lg navbar-dark bg-primary" >
+            <a className="navbar-brand" href="/">
+                Employee Directory
+            </a>
+        </div>
+    )
+};
+
+export default Nav;
