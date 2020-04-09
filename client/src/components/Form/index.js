214a9a18715b412a037ac65c2a891fd702481dd5
Moses Palacios <mospalacios@gmail.com>
2020-04-08 23:26:55 -0400

    added form component

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export function Input(props) {
+    return (
+        <div className = "form-group" {...props}>
+            <input className= "form-control" />
+
+        </div>
+    )
+};
